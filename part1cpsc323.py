38b9358468866a2b4969238f6be10f68be58bf6e
karnikaavelumani <karnikaavelumani@csu.fullerton.edu>
2022-04-17 15:57:41 -0700

    progress

--- part1cpsc323.py
+++ part1cpsc323.py
@@ -3,6 +3,9 @@ f = open("finalp2.txt", "w")
 
 line = txt.readline()
 while(len(line)):
+    #removes blank lines
     if line != '\n':
+        #remove spaces in the beginning of the string
+        line = line.lstrip()
         f.write(line)
     line = txt.readline()
